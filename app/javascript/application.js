93e7e63000be709cb86bc60bbf0e1b0bb528f217
JcMastro <jcmastrocarv@gmail.com>
2022-01-21 12:12:18 -0600

    correcion de vistas haml

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,15 +4,3 @@ import "controllers"
 
 //= require jquery
 
-$("[data-form-prepend]").click(function(e) {
-    var obj = $($(this).attr("data-form-prepend"));    
-    obj.find("input, select, textarea").each(function() {    
-        $(this).attr("name", function() {    
-        return $(this)    
-            .attr("name")    
-            .replace("new_record", new Date().getTime());    
-        });    
-    });    
-    obj.insertBefore(this);    
-    return false;    
-});
